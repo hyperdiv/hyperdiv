fa0f44d2e5349c2d1a1ae55f0275d7e6abe426b2
Marius Nita <m@mariusnita.com>
2024-02-28 15:07:16 -0700

    Split media_source into own module.

---
+++ hyperdiv/components/media_source.py
@@ -0,0 +1,14 @@
+from ..component_base import Component
+from ..prop import Prop
+from ..prop_types import PureString
+
+
+class media_source(Component):
+    """
+    A media source usable with @component(video) and @component(audio).
+    """
+
+    _tag = "source"
+
+    # A local or remote path to an audio file.
+    src = Prop(PureString, "")
