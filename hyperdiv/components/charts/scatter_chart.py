0251acc168106e61e0bf89f471fe277d229b9900
Tom Mutimer <tmutimer96@gmail.com>
2024-03-08 14:17:49 +0000

    Add y min and max to scatter params

--- hyperdiv/components/charts/scatter_chart.py
+++ hyperdiv/components/charts/scatter_chart.py
@@ -11,6 +11,8 @@ def scatter_chart(
     hide_legend=False,
     show_x_tick_labels=True,
     show_y_tick_labels=True,
+    y_min=None,
+    y_max=None,
     **kwargs,
 ):
     """
@@ -35,5 +37,7 @@ def scatter_chart(
         hide_legend=hide_legend,
         show_x_tick_labels=show_x_tick_labels,
         show_y_tick_labels=show_y_tick_labels,
+        y_min=y_min,
+        y_max=y_max,
         **kwargs,
     )
