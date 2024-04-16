b68a76338e8815adcbe0bd4b9a4d91ce662ee44e
Marius Nita <m@mariusnita.com>
2024-04-16 14:19:47 -0600

    Add index_page assets tests.

--- hyperdiv/tests/index_page_tests.py
+++ hyperdiv/tests/index_page_tests.py
@@ -1,5 +1,12 @@
+import re
 import pytest
-from ..index_page import index_page
+from ..index_page import (
+    index_page,
+    js_tag_from_dict,
+    js_tag_from_url,
+    css_tag_from_dict,
+    css_tag_from_url,
+)
 
 
 def test_index_page():
@@ -54,3 +61,99 @@ def test_index_page():
 
     page = index_page(twitter_card_type="invalid")
     assert '<meta name="twitter:card" content="summary_large_image" />' in page
+
+
+def test_assets_functions():
+    assert (
+        js_tag_from_url("/assets/script.js")
+        == '<script src="/assets/script.js"></script>'
+    )
+
+    assert (
+        css_tag_from_url("/assets/style.css")
+        == '<link rel="stylesheet" href="/assets/style.css" />'
+    )
+
+    assert (
+        js_tag_from_dict(
+            {
+                "id": "123",
+                "async": True,
+                "type": "module",
+                "src": "/assets/script.js",
+            }
+        )
+        == '<script id="123" async type="module" src="/assets/script.js"></script>'
+    )
+
+    assert (
+        css_tag_from_dict(
+            {
+                "rel": "stylesheet",
+                "referrerpolicy": "origin",
+                "crossorigin": "use-credentials",
+                "href": "/assets/styles.css",
+            }
+        )
+        == '<link rel="stylesheet" referrerpolicy="origin" crossorigin="use-credentials" href="/assets/styles.css" />'
+    )
+
+
+def test_assets_in_index_page():
+    page = index_page(
+        js_assets=["/assets/script.js", "https://foo.com/script.js?x=123"],
+        css_assets=["/assets/styles.css", "https://foo.com/styles.css?x=123"],
+    )
+    assert '<script src="/assets/script.js"></script>' in page
+    assert '<script src="https://foo.com/script.js?x=123"></script>' in page
+    assert '<link rel="stylesheet" href="/assets/styles.css" />' in page
+    assert '<link rel="stylesheet" href="https://foo.com/styles.css?x=123" />' in page
+
+    page = index_page(
+        js_assets=[dict(defer=True, src="/assets/script.js")],
+        css_assets=[
+            dict(
+                rel="stylesheet",
+                crossorigin="use-credentials",
+                href="https://foo.com/styles.css",
+            )
+        ],
+    )
+    assert '<script defer src="/assets/script.js"></script>' in page
+    assert (
+        '<link rel="stylesheet" crossorigin="use-credentials" href="https://foo.com/styles.css" />'
+        in page
+    )
+
+    def normalize_spaces(s):
+        return
+
+    def contains_normalized(substring, string):
+        normalized_substring = re.sub(r"\s+", " ", substring).strip()
+        normalized_string = re.sub(r"\s+", " ", string).strip()
+
+        # Check if normalized substring exists in the normalized string
+        return normalized_substring in normalized_string
+
+    page = index_page(
+        js_assets=["/assets/script1.js"],
+        raw_head_content=(
+            """
+            <script src="/assets/script2.js"></script>
+            <link rel="stylesheet" href="/assets/styles.css" />
+            <script>
+              console.log("Hello World");
+            <script>
+            """
+        ),
+    )
+    assert '<script src="/assets/script2.js"></script>' in page
+    assert '<link rel="stylesheet" href="/assets/styles.css" />' in page
+    assert contains_normalized(
+        """
+        <script>
+          console.log("Hello World");
+        <script>
+        """,
+        page,
+    )
