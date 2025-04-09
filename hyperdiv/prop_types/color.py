ee0351d36a0b4da4530a72673b6a6967c3644a22
Marius Nita <m@mariusnita.com>
2025-04-09 14:12:10 -0600

    New Color implementation that supports color mixing.

---
+++ hyperdiv/prop_types/color.py
@@ -0,0 +1,66 @@
+from .. import design_tokens as tokens
+from .hyperdiv_type import HyperdivType
+from .design_token import DesignToken
+from .color_constant import ColorConstant
+
+ColorToken = DesignToken(tokens.Color)
+
+
+class ColorDef(HyperdivType):
+    def parse(self, value):
+        if value is None:
+            return value
+
+        try:
+            return ColorToken.parse(value)
+        except Exception:
+            pass
+
+        try:
+            return ColorConstant.parse(value)
+        except Exception:
+            pass
+
+        if isinstance(value, tuple) and len(value) == 6 and value[0] == "color-mix":
+            _, c1, p1, c2, p2, cs = value
+            c1 = self.parse(c1)
+            c2 = self.parse(c2)
+            if p1 < 0 or p1 > 1 or p2 < 0 or p1 > 1:
+                raise ValueError("For color percent, values should be between 0 and 1.")
+            if cs not in ("srgb", "hsl"):
+                raise ValueError("Unknown color space.")
+            return ("color-mix", c1, p1, c2, p2, cs)
+
+        raise ValueError(f"Unknown color: {value}")
+
+    def render(self, value):
+        if value is None:
+            return None
+
+        if isinstance(value, tuple) and value[0] == "color-mix":
+            _, c1, p1, c2, p2, cs = value
+            c1 = self.render(c1)
+            c2 = self.render(c2)
+            return f"color-mix(in {cs}, {c1} {p1*100:.2f}%, {c2} {p2*100:.2f}%)"
+
+        try:
+            return ColorToken.render(value)
+        except Exception:
+            pass
+
+        return ColorConstant.render(value)
+
+
+Color = ColorDef()
+
+
+def color_mix(color1, percent1, color2, percent2, color_space="srgb"):
+    return ("color-mix", color1, percent1, color2, percent2, color_space)
+
+
+def darken(color, percent, color_space="srgb"):
+    return ("color-mix", color, 1 - percent, "neutral-0", percent, color_space)
+
+
+def lighten(color, percent, color_space="srgb"):
+    return ("color-mix", color, 1 - percent, "neutral-1000", percent, color_space)
